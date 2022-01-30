6c786e2e74685aab01316a08e81e58d91c1c0d2d
rajkamalnitj <rajkamalk.cs.19@nitj.ac.in>
2022-01-30 07:36:34 +0530

    1 file modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react';
 
- import Topbar from "./Topbar/Topbar"
- import Home from "./Pages/Home/Home"
+import Topbar from './Topbar/Topbar';
+import Single from './Pages/Single/Single'
+import Home from './Pages/Home/Home'
+
+
 export default function App() {
   return (
- <>
-  <Topbar/>
-<Home/>
-  
-   </>
+    <>
+      <Topbar />
+      <Single/>
+    </>
   );
 }
---
+++ src/Pages/Single/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+  import Sidebar from '../../Sidebar/Sidebar'
+   import Singlepost from '../Singlepost/Singlepost'
+    import './Single.css'
+export default function Single() {
+  return (
+<div className='Single'>
+<Singlepost/>
+<Sidebar/>
+  </div>
+  )
+
+}
