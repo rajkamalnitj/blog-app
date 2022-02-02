c090ed03157132be705b2a80ed37223d7652505a
rajkamalnitj <rajkamalk.cs.19@nitj.ac.in>
2022-02-02 05:55:47 +0530

    3 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
 import Topbar from './Topbar/Topbar';
-import Single from './Pages/Single/Single'
-import Home from './Pages/Home/Home'
- import Write from './Pages/Write/Write'
- 
- import Setting from './Pages/Setting/Setting'
- import Login from './Pages/Login/Login'
+import Single from './Pages/Single/Single';
+import Home from './Pages/Home/Home';
+import Write from './Pages/Write/Write';
+
+import Setting from './Pages/Setting/Setting';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+
+import { BrowserRouter as Router,Routes, Switch, Route, Link } from 'react-router-dom';
+
 export default function App() {
   return (
-    <>
+    <Router>
       <Topbar />
       {/* <Single/> */}
       {/* <Write/> */}
       {/* <Setting/> */}
-      <Login/>
-      </>
-  
+<Home/>
+     
+    
+
+
+      
+    </Router>
   );
 }
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,14 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import './Login.css';
-export default function Login() {
+import './Register.css';
+
+export default function Register() {
   return (
-    <div className="Login">
-         <span><b>Login</b></span>
+    <div className="Register">
+
+
+      <span><b>Register</b></span>
       <div className="container">
         <div className="row d-flex justify-content-center">
           <div className=" formwrapper col-md-4">
-            <form id="loginform">
+            <form id="Registerform">
+
+            <div className="form-group">
+                <label>Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="usernameInput"
+                  name="usernameInput"
+                  aria-describedby="usenameHelp"
+                  placeholder="Enter  Username"
+                />
+                <small id="emailHelp" className="text-danger form-text"></small>
+              </div>
               <div className="form-group">
                 <label>Email address</label>
                 <input
@@ -43,15 +59,12 @@ export default function Login() {
                 <label className="form-check-label">Check me out</label>
               </div>
               <div className="BUTTONS">
-              <button type="submit" className="btn ">
-                Login
-              </button>
-              <button type="submit" className="btn">Register</button>
-
-</div>
-
+             
+                <button type="submit" className="btn">
+                  Register
+                </button>
+              </div>
             </form>
-         
           </div>
         </div>
       </div>
